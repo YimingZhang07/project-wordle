f76962a3d2bead5d9d0e92c11176798deee5ed1b
Yiming Zhang <yiming.zhang07@outlook.com>
2025-12-18 22:02:08 -0500

    update

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+function GuessInput() {
+  const [guessTerm, setGuessTerm] = React.useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    // log the uppercase value of the input field to the console
+    console.info({ guessTerm });
+    setGuessTerm('');
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="guess-input-wrapper">
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        value={guessTerm}
+        required
+        minLength={5}
+        maxLength={5}
+        title="Must be exactly 5 characters"
+        onChange={(event) => {
+          // convert input value to uppercase when the user types
+          setGuessTerm(event.target.value.toUpperCase());
+        }}
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
---
+++ src/components/GuessResults/GuessResults.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function GuessResults() {
+  return (
+    <div className="guess-results">
+      <p>Put guess results here!</p>
+      <p>For now, open the console to see your submitted guesses.</p>
+    </div>
+  );
+}
+
+export default GuessResults;
